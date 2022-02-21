e64c720d40ebae0ca8d204ddab9199c85d1cbad8
tha-ninja <ibra5428@gmail.com>
2022-02-21 10:50:52 +0300

    Home TextField, background update

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/Screens/Welcome/components/background.dart';
 
+
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
@@ -10,14 +11,26 @@ class Body extends StatelessWidget {
     //Gets Total Height and Width of our screen
     Size size = MediaQuery.of(context).size;
     return Background(
+
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        Text(
-            "Welcome To KEOnline",
-            style: TextStyle(fontWeight: FontWeight.bold),
-        )
+        // Image.asset("assets/images/logo.png",
+        //     width: size.width * 0.25,
+        //     height: size.height * 0.6),
+
+
+        TextField(
+          decoration: InputDecoration(
+            suffixIcon: Icon(Icons.search),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            labelText: "Search Companies, Brands & Businesses",
+            width:
+          ),
+        ),
       ],
     ));
   }
